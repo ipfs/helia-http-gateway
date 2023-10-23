72e130554014044f044e67c2a82a1c1dda8b5947
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-10-23 11:26:04 -0700

    feat: move HOST,PORT to src/constants.ts

--- src/healthcheck.ts
+++ src/healthcheck.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+import { PORT } from './constants.js'
 /**
  * This healthcheck script is used to check if the server is running and healthy.
  */
-const rootReq = await fetch(`http://localhost:${process.env.PORT ?? 8080}`, { method: 'GET' })
+const rootReq = await fetch(`http://localhost:${PORT}`, { method: 'GET' })
 const status = rootReq.status
 // eslint-disable-next-line no-console
 console.log(`Healthcheck status: ${status}`)
