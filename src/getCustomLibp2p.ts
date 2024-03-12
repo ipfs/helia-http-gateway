554f412b1d509c784b5ae20ecd52d6c99b2b7457
achingbrain <alex@achingbrain.net>
2024-03-12 08:00:27 +0100

    feat: update libp2p config and collect libp2p metrics
    Updates the default custom libp2p config to enable missing transports,
    configure the DHT correctly and to collect prometheus metrics along
    with the default fastify ones.

--- src/getCustomLibp2p.ts
+++ src/getCustomLibp2p.ts
@@ -1,29 +1,31 @@
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { createDelegatedRoutingV1HttpApiClient } from '@helia/delegated-routing-v1-http-api-client'
-import { autoNAT as autoNATService } from '@libp2p/autonat'
 import { bootstrap } from '@libp2p/bootstrap'
 import { circuitRelayTransport } from '@libp2p/circuit-relay-v2'
-import { dcutr as dcutrService } from '@libp2p/dcutr'
-import { identify as identifyService, type Identify } from '@libp2p/identify'
-import { type KadDHT, kadDHT } from '@libp2p/kad-dht'
+import { identify, type Identify } from '@libp2p/identify'
+import { type KadDHT, kadDHT, removePrivateAddressesMapper } from '@libp2p/kad-dht'
 import { mplex } from '@libp2p/mplex'
-import { ping as pingService, type PingService } from '@libp2p/ping'
+import { ping } from '@libp2p/ping'
+import { prometheusMetrics } from '@libp2p/prometheus-metrics'
 import { tcp } from '@libp2p/tcp'
+import { tls } from '@libp2p/tls'
+import { webRTC, webRTCDirect } from '@libp2p/webrtc'
 import { webSockets } from '@libp2p/websockets'
 import { ipnsSelector } from 'ipns/selector'
 import { ipnsValidator } from 'ipns/validator'
-import { createLibp2p as create, type Libp2pOptions } from 'libp2p'
-import { DELEGATED_ROUTING_V1_HOST, USE_DELEGATED_ROUTING } from './constants.js'
+import { createLibp2p as create, type Libp2pOptions, type ServiceFactoryMap } from 'libp2p'
+import isPrivate from 'private-ip'
+import { DELEGATED_ROUTING_V1_HOST, METRICS, USE_DELEGATED_ROUTING } from './constants.js'
 import type { Libp2p, ServiceMap } from '@libp2p/interface'
+import type { PingService } from '@libp2p/ping'
+import type { Multiaddr } from '@multiformats/multiaddr'
 import type { HeliaInit } from 'helia'
 
-interface HeliaGatewayLibp2pServices extends Record<string, unknown> {
+interface HeliaGatewayLibp2pServices extends ServiceMap {
   dht: KadDHT
   delegatedRouting: unknown
   identify: Identify
-  autoNAT: unknown
-  dcutr: unknown
   ping: PingService
 }
 
@@ -32,11 +34,11 @@ interface HeliaGatewayLibp2pOptions extends Pick<HeliaInit, 'datastore'> {
 }
 
 export async function getCustomLibp2p ({ datastore }: HeliaGatewayLibp2pOptions): Promise<Libp2p<HeliaGatewayLibp2pServices>> {
-  const libp2pServices: ServiceMap = {
-    identify: identifyService(),
-    autoNAT: autoNATService(),
-    dcutr: dcutrService(),
+  const libp2pServices: ServiceFactoryMap = {
+    identify: identify(),
     dht: kadDHT({
+      protocol: '/ipfs/kad/1.0.0',
+      peerInfoMapper: removePrivateAddressesMapper,
       // don't do DHT server work.
       clientMode: true,
       validators: {
@@ -46,7 +48,7 @@ export async function getCustomLibp2p ({ datastore }: HeliaGatewayLibp2pOptions)
         ipns: ipnsSelector
       }
     }),
-    ping: pingService()
+    ping: ping()
   }
 
   if (USE_DELEGATED_ROUTING) {
@@ -60,37 +62,22 @@ export async function getCustomLibp2p ({ datastore }: HeliaGatewayLibp2pOptions)
         // helia-http-gateway is not dialable, we're only retrieving data from IPFS network, and then providing that data via a web2 http interface.
       ]
     },
-    connectionManager: {
-      /**
-       * disable auto-dial because we don't want to be doing a lot of work to keep connections alive.
-       */
-      minConnections: 0
-    },
     transports: [
-      circuitRelayTransport({
-        discoverRelays: 0
-      }),
+      circuitRelayTransport(),
       tcp(),
-      /**
-       * Temporarily disabling webrtc while waiting on release of:
-       * * https://github.com/libp2p/js-libp2p/pull/2299
-       * * https://github.com/libp2p/js-libp2p/pull/2302
-       *
-       * @see https://github.com/libp2p/js-libp2p/pull/2301
-       */
-      // webRTC(),
-      // webRTCDirect(),
+      webRTC(),
+      webRTCDirect(),
       webSockets()
     ],
     connectionEncryption: [
-      noise()
+      noise(),
+      tls()
     ],
     streamMuxers: [
       yamux(),
       mplex()
     ],
     peerDiscovery: [
-      // mdns(), // disable mdns while optimizing for performance.
       bootstrap({
         list: [
           '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
@@ -101,8 +88,29 @@ export async function getCustomLibp2p ({ datastore }: HeliaGatewayLibp2pOptions)
         ]
       })
     ],
-    // @ts-expect-error - types are borked
-    services: libp2pServices
+    services: libp2pServices,
+    connectionGater: {
+      denyDialMultiaddr: async (multiaddr: Multiaddr) => {
+        const tuples = multiaddr.stringTuples()
+
+        // deny private ip4/ip6 addresses
+        if (tuples[0][0] === 4 || tuples[0][0] === 41) {
+          return Boolean(isPrivate(`${tuples[0][1]}`))
+        }
+
+        // all other addresses are ok
+        return false
+      }
+    }
+  }
+
+  if (METRICS === 'true') {
+    options.metrics = prometheusMetrics({
+      // this is done by fastify-metrics
+      collectDefaultMetrics: false,
+      // do not remove metrics configured by fastify-metrics
+      preserveExistingMetrics: true
+    })
   }
 
   return create(options)
